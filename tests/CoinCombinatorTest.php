68313604ed86eafd62b5c0ce5cd5ef0de647b8e5
Ashlin Aronin & Deron Johnson <student@epicodus.com>
2015-08-11 14:51:28 -0700

    Write penny spec and make it fail

---
+++ tests/CoinCombinatorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+    require_once "src/CoinCombinator.php";
+
+    class CoinCombinatorTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_countCoins_penny()
+        {
+            //Arrange
+            $test_CoinCombinator = new CoinCombinator;
+            $cents = 1;
+
+            //Act
+            $result = $test_CoinCombinator->countCoins($cents);
+
+            //Assert
+            $desired_array = array(
+                "pennies" => 1,
+                "nickels" => 0,
+                "dimes"   => 0,
+                "quarters"=> 0
+            );
+            $this->assertEquals($desired_array, $result);
+        }
+    }
+?>

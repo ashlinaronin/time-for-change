45746053d93b394d71dbadd5fdfa27d3299fd158
Ashlin Aronin & Deron Johnson <student@epicodus.com>
2015-08-11 15:29:39 -0700

    Make one dime test pass

--- src/CoinCombinator.php
+++ src/CoinCombinator.php
@@ -11,13 +11,16 @@
                 "quarters" => 0
             );
 
+            $coins["dimes"] = (int)($cents / 10);
+            $dimes_remainder = $cents % 10;
+
             // # nickels is whole number quotient of 5
             // we must cast it as an int to avoid messy decimals
             // which are dealt with by pennies
-            $coins["nickels"] = (int)($cents / 5);
+            $coins["nickels"] = (int)($dimes_remainder / 5);
 
             // # pennies is remainder on division by 5
-            $coins["pennies"] = $cents % 5;
+            $coins["pennies"] = $dimes_remainder % 5;
 
             return $coins;
         }

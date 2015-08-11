3c0cd5b187f261a50516bfb7b15a10dfc277ce2b
Ashlin Aronin & Deron Johnson <student@epicodus.com>
2015-08-11 15:08:24 -0700

    Write countCoins function to make first test pass

--- src/CoinCombinator.php
+++ src/CoinCombinator.php
@@ -4,7 +4,14 @@
     {
         function countCoins($cents)
         {
-            
+            $coins = array(
+                "pennies" => 0,
+                "nickels" => 0,
+                "dimes" => 0,
+                "quarters" => 0
+            );
+            $coins["pennies"] = $cents % 5;
+            return $coins;
         }
     }
 ?>

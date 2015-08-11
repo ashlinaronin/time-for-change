8d5ee8dcbb308ee6184e0545b1e1e7445fe25de0
Ashlin Aronin & Deron Johnson <student@epicodus.com>
2015-08-11 16:12:50 -0700

    Write route for coin results page

--- app/app.php
+++ app/app.php
@@ -13,5 +13,11 @@
         return $app['twig']->render('cents_input.html.twig');
     });
 
+    $app->get("/coins_result", function() use ($app) {
+        $new_CoinCombinator = new CoinCombinator;
+        $result_coins = $new_CoinCombinator->countCoins($_GET['cents']);
+        return $app['twig']->render('coins_result.html.twig', array('results' => $result_coins));
+    });
+
     return $app;
 ?>
